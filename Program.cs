4c266ac50b91d03eb942efdf46f3024f201818c5
joao vitor <joaovitor.rb19@gmail.com>
2023-03-31 12:19:02 -0300

    Adicionado alguns arquivos ao projeto

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDeveloperExceptionPage();
+
+app.UseRouting();
+
+app.UseStaticFiles();
+
+app.UseEndpoints(options => options.MapDefaultControllerRoute());
 
 app.Run();
